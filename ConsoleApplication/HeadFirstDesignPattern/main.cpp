71fad6e2bc2e07b87f1d26a9e64b8ffb7f861f1b
Princerin <erosnick@gmail.com>
2019-02-19 21:06:45 +0800

    Update.

--- ConsoleApplication/HeadFirstDesignPattern/main.cpp
+++ ConsoleApplication/HeadFirstDesignPattern/main.cpp
@@ -73,7 +73,7 @@ int main()
 	SimpleRemoteControl* remote = new SimpleRemoteControl();
 
 	Light* light = new Light();
-	LightOnCommand* lightOn = new LightOnCommand(light); 
+	LightOnCommand* lightOn = new LightOnCommand(light);
 
 	remote->setCommand(lightOn);
 	remote->buttonWasPressed();
